79cc88ecad4d344198b8c4cf6b866d3283fe9d50
Genelink <bixbysfamily@gmail.com>
2022-09-21 23:52:22 -0700

    PoopFart Test
    It was a joke

---
+++ Space Shooter/Assets/Scripts/PoopyFart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoopyFart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is where the poop starts?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
